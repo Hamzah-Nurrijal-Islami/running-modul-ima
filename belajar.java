711f033edc3664d0194ac43b2df1cee014d9aa1b
Hamzah-Nurrijal-Islami <h4m24hnv1212i@gmail.com>
2024-10-01 12:50:26 +0700

    menambahkan file java pertama

---
+++ belajar.java
@@ -0,0 +1,7 @@
+public class belajar {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Nama saya");
+        System.out.println("Hamzah Nurrijal Islami");
+    }
+}
